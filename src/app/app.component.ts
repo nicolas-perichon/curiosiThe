5808f6ba8b97c6c281cc6fc06c949e7f91941d56
nicolas-perichon <nicolas.perichon.visiteur@lecnam.net>
2023-03-08 13:37:45 +0100

    Premier commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Database, set, ref, update } from '@angular/fire/database';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'curiosiThe';
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBT-L4SlEcB15LO8nASlXu566JJqDRzZ20",
+        authDomain: "curiosithe-283da.firebaseapp.com",
+        databaseURL: "https://curiosithe-283da-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "curiosithe-283da",
+        storageBucket: "curiosithe-283da.appspot.com",
+        messagingSenderId: "442769043350",
+        appId: "1:442769043350:web:732035278a603e82cef7b7",
+        measurementId: "G-QGHR0GWWNW"
+}
+}
